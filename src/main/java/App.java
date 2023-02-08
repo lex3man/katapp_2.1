7eee41535def18fce3aff81f4229846804aeb6ed
lex3man <lex3man@gmail.com>
2023-02-08 13:36:44 +0300

    2.1.2 done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean_hw1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        System.out.println(bean_hw1.getMessage());
+        HelloWorld bean_hw2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean_hw2.getMessage());
+        Cat bean_cat1 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean_cat1.getName());
+        Cat bean_cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(bean_cat2.getName());
     }
 }
